d8b3203bc3f7ed322cb04703155bcf4eb453c580
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2026-08-19 06:42:00 -0300

    init(backend): arquivos iníciais dos microserviços de faturamento e estoque

---
+++ src/Faturamento/Korp.Faturamento.Api/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Korp.Faturamento.Api.Controllers;
+
+[ApiController]
+[Route("/")]
+[Tags("A P I")]
+public sealed class DefaultController : ControllerBase
+{
+    [HttpGet]
+    [EndpointSummary("Checagem")]
+    [EndpointDescription("Verifica se a API está funcional.")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    public async Task<IResult> Get() => TypedResults.Ok("Korp.Faturamento.Api is running!");
+}
