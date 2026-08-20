58099d47b822d323edf5c8471d436226c3ccf306
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2026-08-20 01:50:47 -0300

    feat(backend-faturamento): inclui o result pattern

---
+++ src/Faturamento/Korp.Faturamento.Application/Abstractions/Wrappers/ResultExtensions.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Korp.Faturamento.Domain.Abstractions.Types;
+
+namespace Korp.Faturamento.Application.Abstractions.Wrappers;
+
+[DebuggerNonUserCode]
+[ExcludeFromCodeCoverage]
+public static class ResultExtensions
+{
+    public static TResult Match<TResult>
+        (this Result result, Func<TResult> onSuccess, Func<Error, TResult> onFailure)
+    {
+        return result.IsSuccess ? onSuccess() : onFailure(result.Error!);
+    }
+
+    public static TResult Match<TValue, TResult>(this Result<TValue> result, Func<TValue, TResult> onSuccess, Func<Error, TResult> onFailure)
+    {
+        return result.IsSuccess ? onSuccess(result.Value) : onFailure(result.Error!);
+    }
+}
